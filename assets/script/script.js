23ed756004f9eade5895812720724e4533c4b952
Jamie Bennett <jbennett.130602@gmail.com>
2025-01-22 19:15:27 +0000

    add splash screen

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,7 +1,32 @@
-const menuIcon = document.querySelector('#menu-icon');
-const navbar = document.querySelector('.navbar');
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector(".navbar");
 
-menuIcon.addEventListener('click', () => {
-    menuIcon.classList.toggle('fa-x');
-    navbar.classList.toggle('active');
+menuIcon.addEventListener("click", () => {
+  menuIcon.classList.toggle("fa-x");
+  navbar.classList.toggle("active");
+});
+
+let intro = document.querySelector(".intro");
+let introText = document.querySelector(".intro-header");
+let introSpan = document.querySelectorAll(".intro-text");
+
+window.addEventListener("DOMContentLoaded", () => {
+  setTimeout(() => {
+    introSpan.forEach((span, idx) => {
+      setTimeout(() => {
+        span.classList.add("active");
+      }, (idx + 1) * 400);
+    });
+    setTimeout(() => {
+      introSpan.forEach((span, idx) => {
+        setTimeout(() => {
+          span.classList.remove("active");
+          span.classList.add("fade");
+        }, (idx + 1) * 50);
+      });
+    }, 2000);
+    setTimeout(() => {
+        intro.style.top = '-100vh';
+    }, 2300)
+  });
 });
